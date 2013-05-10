68a0f469b9db6f98bf9fc369ea28166081d4c166
dbsima <simadragosbogdan@gmail.com>
2013-05-10 17:06:30 +0300

    show keypoints of the real image

--- gestures.py
+++ gestures.py
@@ -1,4 +1,5 @@
 import SimpleCV
+from SimpleCV import Image, Color, Display
 
 # start local webcam
 camera = SimpleCV.Camera()
@@ -6,6 +7,18 @@ camera = SimpleCV.Camera()
 while 1:
 	# load an image from webcam
 	image = camera.getImage()
+
+	# use a keypoint detector to find areas of interest
+	feats = image.findKeypoints()
+
+	# draw the list of keypoints
+        feats.draw(color=Color.RED)
+
+        # apply the stuff we found to the image.
+        output = image.applyLayers()
+
+        # save the results
+        output.save("test.png")
 	
 	# show the  resulting image.
 	image.show()
